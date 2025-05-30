9d0a9f1211d25e18c4651aa6e41dcd08b017b488
Dior Hamja <dior.hamja@gmail.com>
2025-05-30 18:31:17 +0200

    Part 1

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ProductForm from './components/ProductForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <ProductForm/>
+    </div>
+  )
+}
+
+export default App
